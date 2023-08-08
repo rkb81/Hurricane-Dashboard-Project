469d136b76995f76a9639b26a74965e39e1fe292
Deepika-GH <dpitchikala@yahoo.com>
2023-08-08 18:56:11 -0400

    Tuesday changes

--- plot.js
+++ plot.js
@@ -1,19 +1,25 @@
-const url = 'http://127.0.0.1:5000/api/v1.0/HurricaneLocationData'; 
+// Define the URL to fetch hurricane data from the API
+const url = 'http://127.0.0.1:5000/api/v1.0/HurricaneLocationData';
 
+// Global variables for the map and filters
 let map;
 let markersLayer;
 let hurricanes;
 let selectedYear;
 let selectedHurricane;
 let selectedCountry;
-let defaultMarkers = []; 
 let defaultPolylines = [];
 
+// Initialize the map and UI elements
 function initMap() {
-  map = L.map('map').setView([19, -99], 5); // Set initial map view
+  // Create a Leaflet map with initial view and add a tile layer
+  map = L.map('map').setView([19, -99], 5);
   L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+
+  // Create a layer group for markers and add to the map
   markersLayer = L.layerGroup().addTo(map);
 
+  // Create filter dropdowns and reset button
   const yearSelect = document.getElementById('yearSelect');
   const hurricaneSelect = document.getElementById('hurricaneSelect');
   const countrySelect = document.getElementById('countrySelect');
@@ -22,17 +28,17 @@ function initMap() {
   resetButton.addEventListener('click', resetFilters);
   document.body.appendChild(resetButton);
 
-  // Initialize the filter variables to null
+  // Initialize filter variables to null
   selectedYear = null;
   selectedHurricane = null;
   selectedCountry = null;
-  
+
+  // Fetch unique years, hurricane names, and countries from data
   const years = [...new Set(hurricanes.map(d => new Date(d.Datetime).getFullYear()))];
   const hurricaneNames = [...new Set(hurricanes.map(d => d.Name))];
   const countries = [...new Set(hurricanes.map(d => d.country))];
 
-
-  // Populate year and hurricane select dropdowns
+  // Populate filter dropdowns with options
   years.forEach(year => {
     const option = document.createElement('option');
     option.value = year;
@@ -47,7 +53,6 @@ function initMap() {
     hurricaneSelect.appendChild(option);
   });
 
-  // Populate country select dropdown
   countries.forEach(country => {
     const option = document.createElement('option');
     option.value = country;
@@ -55,180 +60,104 @@ function initMap() {
     countrySelect.appendChild(option);
   });
 
+  // Add event listeners to filter dropdowns
   yearSelect.addEventListener('change', updateMap);
   hurricaneSelect.addEventListener('change', updateMap);
-  countrySelect.addEventListener('change', updateMap);;
-  // hurricaneDropdown.addEventListener('change', updateHurricaneInfo);
-  
-  updateAll(); 
-  
+  countrySelect.addEventListener('change', updateMap);
+
+  // Update the map with initial data
+  updateAll();
 }
 
-function updateMap() {
 
+// Update the map based on selected filters
+function updateMap() {
+  // Get selected filter values and the "No Data" message element
   selectedYear = parseInt(yearSelect.value);
   selectedHurricane = hurricaneSelect.value;
   selectedCountry = countrySelect.value;
   const noDataMessage = document.getElementById('noDataMessage');
 
-  if (map && markersLayer) {
-    markersLayer.clearLayers(); // Clear existing markers
+  // Clear existing markers
+  markersLayer.clearLayers();
 
-    // Check if "All" is selected for both year and hurricane name
-    //const isAllSelected = selectedYear === 'All' && selectedHurricane === 'All';
-    // Check if the selected year is 'All'
-    const isAllYearsSelected = selectedYear === 'All';
-    const isAllHurricanesSelected = selectedHurricane === 'All';
-    const isAllCountriesSelected = selectedCountry === 'All';
-    
-    const filteredData = hurricanes.filter(h => {
-      const year = new Date(h.Datetime).getFullYear();
-      const name = h.Name;
-      const country = h.country || 'Unknown';
+  // Check if "All" is selected for each filter
+  const isAllYearsSelected = selectedYear === 'All';
+  const isAllHurricanesSelected = selectedHurricane === 'All';
+  const isAllCountriesSelected = selectedCountry === 'All';
 
-      // Check if each hurricane matches the selected filters
+  // Filter data based on selected filters
+  const filteredData = hurricanes.filter(h => {
+    const year = new Date(h.Datetime).getFullYear();
+    const name = h.Name;
+    const country = h.country || 'Unknown';
+
+    // Check if each hurricane matches the selected filters
     const yearMatch = isAllYearsSelected || year === selectedYear;
     const hurricaneMatch = isAllHurricanesSelected || name === selectedHurricane;
     const countryMatch = isAllCountriesSelected || country === selectedCountry;
+
     return yearMatch && hurricaneMatch && countryMatch;
-    });
+  });
 
-  // Clear existing markers and default polylines
-   markersLayer.clearLayers();
-  defaultPolylines.forEach(polyline => markersLayer.removeLayer(polyline));
-    
-  if (filteredData.length === 0) {
-      noDataMessage.style.display = 'block';
-    } else {
-      noDataMessage.style.display = 'none';
-    }
+  // Update the map with filtered data
+  updateMarkersAndPolylines(filteredData);
 
-    filteredData.forEach(h => {
-      const year = new Date(h.Datetime).getFullYear();
-      const name = h.Name;
-      const country = h.country || 'Unknown';
+  // Display "No Data" message if no hurricanes match the filters
+  noDataMessage.style.display = filteredData.length === 0 ? 'block' : 'none';
+}
 
-      const latLngs = hurricanes
-      .filter(d => d.Name === name)
+// Update markers and polylines on the map
+function updateMarkersAndPolylines(data) {
+  // Iterate through filtered data and add markers and polylines
+  data.forEach(h => {
+    const latLngs = hurricanes
+      .filter(d => d.Name === h.Name)
       .map(d => [d.Latitude, d.Longitude]);
 
     const polyline = L.polyline(latLngs, { color: getRandomColor() })
-      .bindPopup(`Year: ${year}<br>Name: ${name}<br>Country: ${country}`);
-
+      .bindPopup(`Name: ${h.Name}`);
     markersLayer.addLayer(polyline);
   });
 }
-}
-// function updateAll() {
-//   const yearSelect = document.getElementById('yearSelect');
-//   const hurricaneSelect = document.getElementById('hurricaneSelect');
 
-//   // Set the selected values for the dropdowns to "All"
-//   yearSelect.value = 'All';
-//   hurricaneSelect.value = 'All'
-//   console.log(hurricanes);
-//   if (map && markersLayer) {
-//     markersLayer.clearLayers(); // Clear existing markers
-
-//     hurricanes.forEach(h => {
-//       const year = new Date(h.Datetime).getFullYear();
-//       const name = h.Name;
-//       const country = h.country || 'Unknown';
-
-//       //if (year === selectedYear && (selectedHurricane === 'All' || name === selectedHurricane)) {
-//         L.marker([h.Latitude, h.Longitude])
-//           .bindPopup(`Year: ${year}<br>Name: ${name}<br>Country: ${country}`)
-//           .addTo(markersLayer); // Add the marker to the markersLayer instead of directly to the map
-//       }
-//     //}
-//     );
-//   }
-// }
-
+// Update all markers and polylines for default view
 function updateAll() {
-  // const yearSelect = document.getElementById('yearSelect');
-  // const hurricaneSelect = document.getElementById('hurricaneSelect');
-  // const selectedCountry = countrySelect.value;
-  selectedYear = parseInt(yearSelect.value); // Assign the value to selectedYear
-  selectedHurricane = hurricaneSelect.value;
-  selectedCountry = countrySelect.value;
+  selectedYear = 'All';
+  selectedHurricane = 'All';
+  selectedCountry = 'All';
 
-  selectedYear = 'null';
-  selectedHurricane = 'null';
-  selectedCountry = 'null';
-
-
-  // // Set the selected values for the dropdowns to "All"
-  // yearSelect.value = 'All';
-  // hurricaneSelect.value = 'All';
-  // countrySelect.value = 'All';
-
-  // Clear the default polylines
+  // Clear existing markers and polylines
+  markersLayer.clearLayers();
   defaultPolylines.forEach(polyline => markersLayer.removeLayer(polyline));
   defaultPolylines = [];
-  
-  //  defaultMarkers.forEach(marker => {
-  //   markersLayer.removeLayer(marker);
-  // });
-  // defaultMarkers = [];
 
-  if (map && markersLayer) {
-    markersLayer.clearLayers(); 
-    
-    const groupedHurricanes = groupHurricanesByName(hurricanes);
-      for (const name in groupedHurricanes) {
-        const latLngs = groupedHurricanes[name].map(d => [d.Latitude, d.Longitude]);
-        const polyline = L.polyline(latLngs, { color: getRandomColor() })
-          .bindPopup(generatePopupContent(groupedHurricanes[name]));
+  // Group hurricanes by name and update map
+  const groupedHurricanes = groupHurricanesByName(hurricanes);
+  for (const name in groupedHurricanes) {
+    const latLngs = groupedHurricanes[name].map(d => [d.Latitude, d.Longitude]);
+    const polyline = L.polyline(latLngs, { color: getRandomColor() })
+      .bindPopup(`Name: ${name}`);
 
-        defaultPolylines.push(polyline);
-        markersLayer.addLayer(polyline);
-      }
+    defaultPolylines.push(polyline);
+    markersLayer.addLayer(polyline);
+  }
+}
+
+// Group data by hurricane name
+function groupHurricanesByName(data) {
+  const hurricanesGrouped = {};
+  data.forEach(h => {
+    const name = h.Name;
+    if (!hurricanesGrouped[name]) {
+      hurricanesGrouped[name] = [];
     }
-  }
-  
-  // Clear existing markers
+    hurricanesGrouped[name].push(h);
+  });
+  return hurricanesGrouped;
+}
 
-    // const filteredData = hurricanes.filter(h => {
-    //   const year = new Date(h.Datetime).getFullYear();
-    //   const name = h.Name;
-    //   const country = h.country || 'Unknown';
-
-    //   return (
-    //     (selectedYear === 'All' || year === selectedYear) &&
-    //     (selectedHurricane === 'All' || name === selectedHurricane) &&
-    //     (selectedCountry === 'All' || country === selectedCountry)
-    //   );
-    // });
-
-
-    // Group data by hurricane name
-    function groupHurricanesByName(hurricanes) {
-    const hurricanesGrouped = {};
-    hurricanes.forEach(h => {
-      const name = h.Name;
-      if (!hurricanesGrouped[name]) {
-        hurricanesGrouped[name] = [];
-      }
-      hurricanesGrouped[name].push(h);
-    });
-    return hurricanesGrouped;
-  }
-
-//     // Create lines connecting the points for each hurricane
-//     for (const name in hurricanesGrouped) {
-//       const hurricaneData = hurricanesGrouped[name];
-//       const latLngs = hurricaneData.map(h => [h.Latitude, h.Longitude]);
-//       const polyline = L.polyline(latLngs, { color: getRandomColor() }).addTo(map);
-
-//       // Add popup to the polyline with detailed information
-//       const popupContent = generatePopupContent(hurricaneData);
-//       polyline.bindPopup(popupContent);
-//     }
-//   }
-// }
-
-// Helper function to generate random colors for each hurricane path
+// Generate a random color
 function getRandomColor() {
   const letters = '0123456789ABCDEF';
   let color = '#';
@@ -238,67 +167,40 @@ function getRandomColor() {
   return color;
 }
 
-function generatePopupContent(hurricaneData) {
-  let content = '<div class="popup-content"><b>Hurricane Information:</b><br>';
-  hurricaneData.forEach(h => {
-    const country = h.country || 'Unknown';
-    const name = h.Name;
-    content += `<b>Country:</b> ${country}<br>`;
-    content += `<b>Hurricane Name:</b> ${name}<br>`;
-  });
-  content += '</div>';
-  return content;
-}
+// Reset all filters and update the map
 function resetFilters() {
-//   // Reset all filters to default
-//   selectedYear = 'null';
-//   selectedHurricane = 'null';
-//   selectedCountry = 'null';
-  // Reset all filters to default
   selectedYear = 'All';
   selectedHurricane = 'All';
   selectedCountry = 'All';
 
-  // Set the selected values for the dropdowns to "All"
+  // Set the selected values for the filter dropdowns to "All"
   yearSelect.value = 'All';
   hurricaneSelect.value = 'All';
   countrySelect.value = 'All';
 
-  // Clear the existing markers and default polylines
+  // Clear markers and polylines
   markersLayer.clearLayers();
   defaultPolylines.forEach(polyline => markersLayer.removeLayer(polyline));
   defaultPolylines = [];
 
-  // // Re-add the default polylines representing all hurricanes
-  // const groupedHurricanes = groupHurricanesByName(hurricanes);
-  // for (const name in groupedHurricanes) {
-  //   const latLngs = groupedHurricanes[name].map(d => [d.Latitude, d.Longitude]);
-  //   const polyline = L.polyline(latLngs, { color: getRandomColor() })
-  //     .bindPopup(generatePopupContent(groupedHurricanes[name]));
-    
-  //   defaultPolylines.push(polyline);
-  //   markersLayer.addLayer(polyline);
-  // }
-
-  // Call updateAll to display the default map view with all hurricanes
+  // Update the map with the default view
   updateAll();
-  // Hide the "No data" message when filters are reset
+
+  // Hide the "No Data" message
   const noDataMessage = document.getElementById('noDataMessage');
   noDataMessage.style.display = 'none';
-
-  // Update the map with the reset filters
-  //updateMap();
-
 }
 
+// Fetch hurricane data and initialize the map on page load
 document.addEventListener('DOMContentLoaded', () => {
   d3.json(url).then(data => {
-   hurricanes = data.data;
-   
-   initMap(); 
+    hurricanes = data.data;
 
-   // Add event listener to the reset button
-   const resetButton = document.getElementById('resetFiltersButton');
-   resetButton.addEventListener('click', resetFilters);
+    // Initialize the map and UI elements
+    initMap();
+
+    // Add event listener to the reset button
+    const resetButton = document.getElementById('resetFiltersButton');
+    resetButton.addEventListener('click', resetFilters);
   });
-})
+});
